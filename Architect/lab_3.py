d87a98dfaececec4bd75732af4e4e8435df523af
Marx <relictpriest@gmail.com>
2025-03-15 22:15:47 +0300

    lab 3

---
+++ Architect/lab_3.py
@@ -0,0 +1,15 @@
+def some_logic_in_progress():
+    a = 33
+    b = 58
+    c = 43
+
+    bac = (b**a)**c
+
+    print(bac.__str__())
+    return True
+
+result = some_logic_in_progress()
+
+print('Here will be some Lab results ...')
+print('... may be later... ')
+print('Trust ? -', result.__str__())
